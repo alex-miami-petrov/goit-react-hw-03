79fdbbc77f75eb82e5356e2505bd3dd2b2353d37
Miami <alex.miami.petrov@gmail.com>
2024-09-04 13:48:49 +0300

    m

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Contact from "../Contact";
+import s from "./ContactList/ContactList.module.css";
+
+const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul className={s.contact - list}>
+    {contacts.map((contact) => (
+      <li key={contact.id} className={s.contact - list - item}>
+        <Contact name={contact.name} number={contact.number} />
+        <button onClick={() => onDeleteContact(contact.id)}>Delete</button>
+      </li>
+    ))}
+  </ul>
+);
+
+export default ContactList;
