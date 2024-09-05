5302aee863b639e8ff49ca07b0b75741f2de87b0
Miami <alex.miami.petrov@gmail.com>
2024-09-05 11:14:27 +0300

    m

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import s from "./Contact.module.css";
 
-const Contact = ({ name, number }) => {
+const Contact = ({ name, number, onDelete }) => {
   return (
     <div className={s.contactItem}>
       <p className={s.contactName}>{name}</p>
       <p className={s.contactNumber}>{number}</p>
+      <button onClick={onDelete}>Delete</button>
     </div>
   );
 };
