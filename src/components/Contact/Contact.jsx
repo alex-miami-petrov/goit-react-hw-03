472efb6f0267e7e6fb813816b6cedceb57405c98
Miami <alex.miami.petrov@gmail.com>
2024-09-05 11:26:21 +0300

    m

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,8 +4,11 @@ import s from "./Contact.module.css";
 const Contact = ({ name, number, onDelete }) => {
   return (
     <div className={s.contactItem}>
-      <p className={s.contactName}>{name}</p>
-      <p className={s.contactNumber}>{number}</p>
+      <div className={s.contactWrap}>
+        <p className={s.contactName}>{name}</p>
+        <p className={s.contactNumber}>{number}</p>
+      </div>
+
       <button onClick={onDelete}>Delete</button>
     </div>
   );
